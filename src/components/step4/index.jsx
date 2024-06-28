6842906b8f802cc88b255d2482797ca9ec081438
Nico Kestel <nico.kestel@visusadvisory.com>
2024-06-28 15:40:18 +0200

    fixed invisible top5 scores

--- src/components/step4/index.jsx
+++ src/components/step4/index.jsx
@@ -191,14 +191,14 @@ export default function Step4({ setStepIndex, selectedCategories }) {
     console.log("calcularion", calcularion);
 
     const apiUrl =
-      "http://127.0.0.1:8000/send-email"
-    //"https://visus-process-excellence-index-backend.azurewebsites.net/send-email"; // Replace with your API endpoint
+      // "http://127.0.0.1:8000/send-email"
+      "https://visus-process-excellence-index-backend.azurewebsites.net/send-email"; // Replace with your API endpoint
     toast.success(`Sending data to API:, ${apiUrl}!`);
     fetch(apiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "X_API_KEY": "1234"
+        "X_API_Key": "1234"
       },
       body: JSON.stringify(calcularion), // Convert scoresArray to JSON string
     })
@@ -259,7 +259,7 @@ export default function Step4({ setStepIndex, selectedCategories }) {
             <div className="2xl:mt-[20px] mt-[10px]">
               {sortedScores.map((item, index) => (
                 <div
-                  key={item.process}
+                  key={item.Process}
                   className="p-[3px] rounded-full w-[100%] bg-[black] gradient-border2"
                 >
                   <div className="flex items-center justify-between">
@@ -269,7 +269,7 @@ export default function Step4({ setStepIndex, selectedCategories }) {
                         style={{ backgroundColor: rankTextColors[index] }}
                       />
                       <p className="text-[#F2E9D8] font-medium sm:text-base text-[10px] sm:ml-[14px] ml-[5px]">
-                        {item.process}
+                        {item.Process}
                       </p>
                     </div>
                     <div
